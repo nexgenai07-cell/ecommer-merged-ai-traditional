cbf0d69b0f1b10968634feca0cc2681aa91ed26f
hudaakhter09-cloud <hudaakhter09@gmail.com>
2026-08-27 11:04:06 +0100

    added

--- backend/apps/users/firebase.py
+++ backend/apps/users/firebase.py
@@ -1,6 +1,12 @@
+import json
+import os
 import firebase_admin
 from firebase_admin import credentials
 
 if not firebase_admin._apps:
-    cred = credentials.ApplicationDefault()
+    firebase_credentials = json.loads(
+        os.environ["FIREBASE_CREDENTIALS_JSON"]
+    )
+
+    cred = credentials.Certificate(firebase_credentials)
     firebase_admin.initialize_app(cred)
